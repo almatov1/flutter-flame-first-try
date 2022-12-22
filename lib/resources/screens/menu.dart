31aca054bffdc9e6261afe76773a6460ca059d5b
Almatov Azamat <almatovalshin@gmail.com>
2022-12-22 17:54:48 +0500

    ready to socket

--- lib/resources/screens/menu.dart
+++ lib/resources/screens/menu.dart
@@ -1,8 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_231122_gae/resources/core.dart';
+import 'package:socket_io_client/socket_io_client.dart';
 
-class Menu extends StatelessWidget {
+class Menu extends StatefulWidget {
   const Menu({Key? key}) : super(key: key);
+
+  @override
+  State<Menu> createState() => _MenuState();
+}
+
+class _MenuState extends State<Menu> {
+  @override
+  void initState() {
+    super.initState();
+
+    // TEST SOCKET
+    var socket = io(
+        'http://localhost:3000?serverToken=r43xv43vi&userName=aza',
+        OptionBuilder()
+            .enableForceNewConnection()
+            .disableAutoConnect()
+            .build());
+
+    socket.connect();
+
+    socket.onConnectError((data) {
+      print('fatal error $data');
+      socket.disconnect();
+    });
+
+    socket.on('connectResult', (data) {
+      if (data == true) {
+        print('succes connected');
+      } else {
+        print('error: $data');
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
